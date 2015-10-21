9b9f56e72f1813f63205de01f09525605a842f17
Dustin Guthrie <guthrie.dustin@gmail.com>
2015-10-20 21:17:02 -0400

    additional comments and example

--- speedreader.js
+++ speedreader.js
@@ -21,6 +21,14 @@ var wordCount = allStrings.length;
 //hard mode would be:
 //var total = Math.round(wordCount/230);
 
+//Here is the instructor way to change words per minute
+//function readTime(wpm) {
+//if(typeof wpm !=== "number") return "Sorry, must be a number";
+// var wpm = wpm || 230;
+// return wpm;
+// }
+//readTime (500);
+
 //this takes how many words per minute / # of words in all the P's then round up or down to a whole number
 var total = Math.round(wordCount/howManyWords);
 
@@ -54,4 +62,23 @@ document.querySelector('h3').innerHTML += timeToRead();
 // };
 // document.querySelector('h3').innerHTML += timeToRead();
 
+/////****************************************
+//Here is another attempt at a different way of getting the total
+//var x = document.body.textContent;
+//
 /////******************************************
+//This is the instructor way of making sure the DOM is ready
+// function ready(fn) {
+//   if(typeof fn !== 'function') return;
+//   if(document.readyState === "complete" || document.readyState === "interactive")
+// }
+// return fn();
+// }
+// ready(function () {
+//   var childArr = [].slice.call(document.body.children);
+//
+//   readTime.configWPM(500);
+//
+//   document.querySelector('h3'-reading-time).innerHTML = readTime.words(childArr);
+//
+// })
